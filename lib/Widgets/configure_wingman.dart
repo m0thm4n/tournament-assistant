870433cc0914262a11e3af61e888ed68915ab774
Nathan Moritz <nathan.moritz@protonmail.com>
2023-08-21 10:25:16 -0400

    Refactoring Wingman widget

--- lib/Widgets/configure_wingman.dart
+++ lib/Widgets/configure_wingman.dart
@@ -1,17 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:tournament_assistant/Widgets/wingman.dart';
 
-class ConfigureWingman extends StatefulWidget {
-  ConfigureWingman({super.key});
-
-  @override
-  State<ConfigureWingman> createState() => _ConfigureWingmanState();
-}
-
-class _ConfigureWingmanState extends State<ConfigureWingman> {
+class ConfigureWingman extends HookWidget {
   List<String> selectedItemValue = [""];
   List<TextEditingController> _controllers = <TextEditingController>[];
   List<String> names = ["NONE"];
+  Map<String, dynamic> mappedPlayer = {};
 
   @override
   void initState() {
@@ -23,18 +19,27 @@ class _ConfigureWingmanState extends State<ConfigureWingman> {
       }
     }
     names.removeAt(0);
-
-    super.initState();
   }
 
-  void sendIt() {
+  void sendIt(List<TextEditingController> controllers) {
+    // for (var i = 0; i < 16; i++) {
+    //   var controller = _controllers[i];
+    //   names.add(controller.text);
+    // }
+    //
+    // for (var i = 0; i < names.length; i++) {
+    //   WingmanNames.add(names[i]);
+    // }
     for (var i = 0; i < 16; i++) {
       var controller = _controllers[i];
-      names.add(controller.text);
+      // names.add(controller.text);
+      mappedPlayer.addAll({"player$i": controller.text});
     }
 
     for (var i = 0; i < names.length; i++) {
-      WingmanNames.add(names[i]);
+      FirebaseFirestore.instance.collection('wingmanParticipants').add(
+        mappedPlayer,
+      );
     }
   }
 
@@ -75,7 +80,9 @@ class _ConfigureWingmanState extends State<ConfigureWingman> {
               controller: _controllers[index],
             );
           }),
-      floatingActionButton: ElevatedButton(child: Text("SEND IT!"), onPressed: sendIt, style: ButtonStyle(
+      floatingActionButton: ElevatedButton(child: Text("SEND IT!"), onPressed: () {
+        sendIt(_controllers);
+      }, style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all(
               Theme.of(context).colorScheme.background),
           foregroundColor: MaterialStateProperty.all(
--- lib/Widgets/wingman.dart
+++ lib/Widgets/wingman.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:tournament_assistant/Class/submit.dart';
 import 'package:tournament_assistant/Widgets/changes_names.dart';
@@ -25,6 +26,7 @@ List<String> WingmanNames = [
 
 class Wingman extends StatefulWidget {
   var numOfPlayers;
+  late CollectionReference<Map<String, dynamic>> wingmanPlayers;
 
   Wingman();
 
@@ -42,7 +44,9 @@ class _WingmanState extends State<Wingman> {
 
   var numOfPlayers = 16;
 
-  List<DropdownMenuItem<String>> _dropDownItem() {
+  Future<List<DropdownMenuItem>> _dropDownItem() async {
+    List<DropdownMenuItem> items = [];
+
     // List<String> ddl = [
     //   "NONE",
     //   'ZebraOnCrack',
@@ -63,14 +67,30 @@ class _WingmanState extends State<Wingman> {
     //   'Spoody'
     // ];
 
-    return WingmanNames.map((value) => DropdownMenuItem(
-          value: value,
-          child: Text(value),
-        )).toList();
+    // return WingmanNames.map((value) => DropdownMenuItem(
+    //       value: value,
+    //       child: Text(value),
+    //     )).toList();
+
+    CollectionReference<Map<String, dynamic>> wingmanPlayers = FirebaseFirestore.instance.collection('wingmanParticipants');
+
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await wingmanPlayers.get();
+
+    // Get data from docs and convert map to List
+    final List<Map<String, dynamic>> data = querySnapshot.docs.map((doc) => <String, dynamic>{'Player1': doc['Player1'], 'Player2': doc['Player2'], 'Player3': doc['Player3'], 'Player4': doc['Player4'], 'Player5': doc['Player5'], 'Player6': doc['Player6'], 'Player7': doc['Player7'], 'Player8': doc['Player8'], 'Player9': doc['Player9'], 'Player10': doc['Player10'], 'Player11': doc['Player11'], 'Player12': doc['Player12'], 'Player13': doc['Player13'], 'Player14': doc['Player14'], 'Player15': doc['Player15'], 'Player16': doc['Player16']}).toList();
+
+    for (var i = 0; i < data.length; i++) {
+      items.add(DropdownMenuItem(value: data[i]["player$i"], child: Text(data[i]["player$i"])));
+    }
+
+    return items;
   }
 
   @override
   Widget build(BuildContext context) {
+    List<DropdownMenuItem> items = [];
+
     return Scaffold(
       body: ListView.builder(
           itemCount: numOfPlayers,
@@ -106,7 +126,7 @@ class _WingmanState extends State<Wingman> {
                       WingmanNames.remove(value);
                       setState(() {});
                     },
-                    items: _dropDownItem(),
+                    items: _dropDownItem()
                   )
                 ],
               ),
