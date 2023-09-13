ef1744df483a7ad64d4858fa6670e14794c217a6
Nathan Moritz <nateim3@gmail.com>
2023-09-13 08:32:31 -0400

    Added new feature

--- lib/Widgets/wingman.dart
+++ lib/Widgets/wingman.dart
@@ -44,8 +44,45 @@ class _WingmanState extends State<Wingman> {
 
   var numOfPlayers = 16;
 
-  Future<List<DropdownMenuItem>> _dropDownItem() async {
-    List<DropdownMenuItem> items = [];
+  Future<List<Map<String, dynamic>>> _getWingmanPlayer() async {
+    CollectionReference<Map<String, dynamic>> wingmanPlayers =
+        FirebaseFirestore.instance.collection('wingmanParticipants');
+
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await wingmanPlayers.get();
+
+    // Get data from docs and convert map to List
+    final List<Map<String, dynamic>> data = querySnapshot.docs
+        .map((doc) => <String, dynamic>{
+              'Player1': doc['player0'],
+              'Player3': doc['player1'],
+              'Player2': doc['player2'],
+              'Player4': doc['player3'],
+              'Player5': doc['player4'],
+              'Player6': doc['player5'],
+              'Player7': doc['player6'],
+              'Player8': doc['player7'],
+              'Player9': doc['player8'],
+              'Player10': doc['player9'],
+              'Player11': doc['player10'],
+              'Player12': doc['player11'],
+              'Player13': doc['player12'],
+              'Player14': doc['player13'],
+              'Player15': doc['player14'],
+              'Player16': doc['player15'],
+            })
+        .toList();
+
+    print("DATA: $data");
+
+    return data;
+  }
+
+  List<DropdownMenuItem<String>> _dropDownItem() {
+    List<DropdownMenuItem<String>> items = [];
+    List<Map<String, dynamic>> data = [{}];
+
+    data.removeAt(0);
 
     // List<String> ddl = [
     //   "NONE",
@@ -72,16 +109,15 @@ class _WingmanState extends State<Wingman> {
     //       child: Text(value),
     //     )).toList();
 
-    CollectionReference<Map<String, dynamic>> wingmanPlayers = FirebaseFirestore.instance.collection('wingmanParticipants');
-
-    // Get docs from collection reference
-    QuerySnapshot querySnapshot = await wingmanPlayers.get();
-
-    // Get data from docs and convert map to List
-    final List<Map<String, dynamic>> data = querySnapshot.docs.map((doc) => <String, dynamic>{'Player1': doc['Player1'], 'Player2': doc['Player2'], 'Player3': doc['Player3'], 'Player4': doc['Player4'], 'Player5': doc['Player5'], 'Player6': doc['Player6'], 'Player7': doc['Player7'], 'Player8': doc['Player8'], 'Player9': doc['Player9'], 'Player10': doc['Player10'], 'Player11': doc['Player11'], 'Player12': doc['Player12'], 'Player13': doc['Player13'], 'Player14': doc['Player14'], 'Player15': doc['Player15'], 'Player16': doc['Player16']}).toList();
+    _getWingmanPlayer().then(
+      (value) => setState(() {
+        data = value;
+      }),
+    );
 
     for (var i = 0; i < data.length; i++) {
-      items.add(DropdownMenuItem(value: data[i]["player$i"], child: Text(data[i]["player$i"])));
+      items.add(DropdownMenuItem(
+          value: data[i]["player$i"], child: Text(data[i]["player$i"])));
     }
 
     return items;
@@ -112,22 +148,21 @@ class _WingmanState extends State<Wingman> {
                           color: Theme.of(context).colorScheme.primary)),
                   const Padding(padding: EdgeInsets.fromLTRB(150, 0, 0, 0)),
                   DropdownButton(
-                    value: selectedItemValue[index].toString(),
-                    icon: const Icon(Icons.arrow_downward),
-                    elevation: 16,
-                    style: const TextStyle(color: Colors.black),
-                    underline: Container(
-                      height: 2,
-                      color: Colors.white,
-                    ),
-                    onChanged: (value) {
-                      // This is called when the user selects an item.
-                      selectedItemValue[index] = value!;
-                      WingmanNames.remove(value);
-                      setState(() {});
-                    },
-                    items: _dropDownItem()
-                  )
+                      value: selectedItemValue[index].toString(),
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.black),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.white,
+                      ),
+                      onChanged: (value) {
+                        // This is called when the user selects an item.
+                        selectedItemValue[index] = value!;
+                        WingmanNames.remove(value);
+                        setState(() {});
+                      },
+                      items: _dropDownItem())
                 ],
               ),
             );
@@ -136,10 +171,10 @@ class _WingmanState extends State<Wingman> {
           child: Text("Submit"),
           onPressed: () => {
                 Submit.submitForm(selectedItemValue),
-          Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Done()),
-          ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Done()),
+                ),
               },
           style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(
